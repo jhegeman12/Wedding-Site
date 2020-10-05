8759693077a2fc7a574f5c050c54afd47baa0558
Tom Settle <tsettle44@gmail.com>
2020-10-05 13:35:35 -0500

    new cover img

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ class IndexPage extends React.Component {
                   <div className="column">
                     <p className="nameTitle">TOM SETTLE</p>
                     <p className="descText">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      Loremipsum dolor sit amet, consectetur adipiscing elit,
                       sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                       ullamco laboris nisi ut aliquip ex ea commodo consequat.
