38a9a5e50c6d86d798774452df33a5b74d74cc69
Tom Settle <tsettle44@gmail.com>
2021-02-09 22:51:08 -0600

    get data from rsvp API

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,14 +1,51 @@
 import React from "react"
+import axios from "axios"
 import '../components/rsvp.css'
+import { faThList } from "@fortawesome/free-solid-svg-icons"
 
-const RSVP = () => (
-  
+class RSVP extends React.Component{
+
+    state = {
+        body: [],
+        firstName: "", 
+        lastName: ""
+    }
     
-    <div className='rsvpPage'>
-        <h1 className='rsvpText'>RSVP</h1>
-        <p className='rsvpDescText'>RSVP Tool in development....</p>
-    </div>
+    handleFirstNameChange = (e) => {
+        this.setState({firstName: e.target.value})
+    }
+
+    handleLastNameChange = (e) => {
+        this.setState({lastName: e.target.value})
+    }
+
+    getGuest = (e) => {
+            e.preventDefault()
+            axios.get(`http://localhost:3000/rsvp/${this.state.firstName}/${this.state.lastName}`)
+            .then(response => {
+                this.setState({"body": response.data})
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
   
-)
+    render() {
+        return (
+            <div className='rsvpPage'>
+                <form onSubmit={this.getGuest}>
+                    <input onChange={this.handleFirstNameChange} value={this.state.firstName}></input>
+                    <input onChange={this.handleLastNameChange} value={this.state.lastName}></input>
+                    <input type="submit" value="Submit" />
+                </form>
+                <div>{this.state.body.map((guest, i) => {
+                    return(
+                    <h1 key={i}>{guest.firstName}</h1>
+                    )
+                })}</div>
+            </div>
+        )
+    }
+}
 
 export default RSVP
