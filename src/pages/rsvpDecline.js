0497408f8865762fd5653de32e04e56623597f34
Tom Settle <tsettle44@gmail.com>
2021-04-19 11:39:02 -0500

    rsvp pages

--- src/pages/rsvpDecline.js
+++ src/pages/rsvpDecline.js
@@ -6,7 +6,12 @@ import '../components/rsvpMessage.css'
 const Decline = () => (
     <div className='containerSuccess .container.is-fullhd columns'>
         <div className="formContainer1 column is-one-third is-offset-one-third">
-            <h1>Sad!</h1>
+            <h1 className="header">We will miss you!</h1>
+            <br />
+            <h1>In the meantime, stay safe and healthy! We hope we will get to see you soon :)</h1>
+            <br />
+            <h1 className='signature'>Love,
+            Tom & Danielle</h1>
             <div className='centerParent'>
                 <Link to='/'>
                     <button className='formButton1'>RETURN HOME</button>
