ec326b41fedbff7d8af4141bfc9d6530d6252e78
jhegeman12 <94722940+jhegeman12@users.noreply.github.com>
2021-11-20 15:58:16 -0600

    combopic

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,16 +129,16 @@ class IndexPage extends React.Component {
             
             <div className="info .container.is-fullhd">
               <div className="container" data-sal="fade-in" data-sal-delay="500" data-sal-easing="ease" data-sal-duration='1000'>
-                <p className="details">WEDDING DETAILS</p>
+                <p className="details">Wedding Details</p>
                 <div className="columns">
                   <div className="column">
-                    <p className="detailsSub">CEREMONY</p>
+                    <p className="detailsSub">Ceremony</p>
                     <p className="detailsText">
                     We will be getting married in the lawn adjacent to the Olympic Valley Lodge at Palisades Ski Resort in Olympic Valley, California. Palisades and the mountains hold a special place for us, as we got engaged at the top of Emigrant Gap lift. Our close friend Robert McComb will be the ceremony officiant, and the ceremony will begin at 4 pm. 
                     </p>
                   </div>
                   <div className="column">
-                    <p className="detailsSub">RECEPTION</p>
+                    <p className="detailsSub">Reception</p>
                     <p className="detailsText">
                     Please join us for cocktail hour starting immediately after the ceremony on the sun deck of the Olympic Valley Lodge. The reception will follow inside the lodge at 6:30 PM. The reception will end promptly at 10 pm. The Palisades Village is within walking distance of the Lodge and home to multiple bars and restaurants for late night drinks and food.  
                     </p>
@@ -147,7 +147,7 @@ class IndexPage extends React.Component {
                     </p>
                   </div>
                   <div className="column">
-                    <p className="detailsSub">DRESSCODE</p>
+                    <p className="detailsSub">Dress</p>
                     <p className="detailsText">
                     We ask that our guests adhere to a formal dress code for the wedding. We recommend bringing a jacket, as the weather can drop to less than 50 after sunset. Although uncommon, it has snowed at this time of year before so please keep an eye on the weather! 
                     </p>
@@ -164,32 +164,34 @@ class IndexPage extends React.Component {
                 <div className="column center">
                   <FontAwesomeIcon icon={faPizzaSlice} className="iconFA" />
                   <p className="timelineTitle">Welcome Ceremony</p>
-                  <p className="timelineTime">10/14, 7:00 PM</p>
+                  <p className="timelineTime">Friday - 10/14</p>
+                  <p className="timelineTime">7:00 PM</p>
                 </div>
                 <div className="column center">
                   <FontAwesomeIcon icon={faRing} className="iconFA" />
                   <p className="timelineTitle">Ceremony Begins</p>
+                  <p className="timelineTime">Saturday - 10/15</p>
                   <p className="timelineTime">4:00 PM</p>
                 </div>
                 <div className="column center">
                   <FontAwesomeIcon icon={faGlassCheers} className="iconFA" />
-                  <p className="timelineTitle">Cocktail Hour</p>
-                  <p className="timelineTime">5:30 PM</p>
+                  <p className="timelineTitle">Cocktails</p>
+                  <p className="timelineTime">4:45 PM</p>
                 </div>
                 <div className="column center">
                   <FontAwesomeIcon icon={faPlaceOfWorship} className="iconFA" />
                   <p className="timelineTitle">Reception</p>
-                  <p className="timelineTime">6:30 PM</p>
+                  <p className="timelineTime">5:30 PM</p>
                 </div>
                 <div className="column center">
                   <FontAwesomeIcon icon={faUtensils} className="iconFA" />
                   <p className="timelineTitle">Dinner</p>
-                  <p className="timelineTime">7:00 PM</p>
+                  <p className="timelineTime">6:00 PM</p>
                 </div>
                 <div className="column center">
                   <FontAwesomeIcon icon={faMusic} className="iconFA" />
                   <p className="timelineTitle">Dance</p>
-                  <p className="timelineTime">8:00 PM</p>
+                  <p className="timelineTime">7:00 PM</p>
                 </div>
               </div>
               <div className='column'></div>
@@ -198,7 +200,7 @@ class IndexPage extends React.Component {
             <div className="container" data-sal="fade-in" data-sal-delay="500" data-sal-easing="ease" data-sal-duration='1000'>
               <div className= 'columns'>
                  <div className="column">
-                    <p className="detailsSub">GETTING HERE</p>
+                    <p className="detailsSub">Getting Here</p>
                     <p className="detailsText">
                     The two closest airports are Sacramento and Reno. To get from either airport to Palisades, we recommend renting a car, but public transportation options are listed below. Once you are in the North Tahoe are (Truckee, Tahoe City, Olympic Valley, Incline Village), the Tahoe Truckee Area REgional Transit (TART) can be used to get to Palisades and explore the North Lake Area.</p>   
                     <br></br>
@@ -209,23 +211,22 @@ class IndexPage extends React.Component {
                     The California Zephyr Amtrak line runs between Reno and Sacramento, with a stop in Truckee, CA, but it does not start directly from the Sacramento or Reno airports. The North Lake Tahoe Express shuttle runs directly from the Reno International Airport to the North Lake Tahoe area.  
                     <p className="acLink"><a rel="noreferrer" href='https://www.amtrak.com/california-zephyr-train' target="_blank">Amtrak - California Zephyr</a></p>
                     <br></br>
-    
                     </p>
                   </div>
                   <div className="column">
                     <p className="detailsSub">Accomodations</p>
                     <p className="detailsText">
-                    There is no preset block of rooms set aside for the wedding, but there is a discounted rate for rooms in the Village at Palisades Tahoe. Please mention the Redovian-Hegeman wedding to receive the discount. If you are not interested in staying at the Palisades lodge, there are many Airbnb and Vrbo rentals available in the area. 
+                    There is a discounted rate for rooms in the Village at Palisades Tahoe. Please mention the Redovian-Hegeman wedding to receive the discount. If you are not interested in staying at the Palisades lodge, there are many Airbnb and Vrbo rentals available in the area. 
                     <br></br>
                     <br></br>
-            
-                    <p className="acLink"><a rel="noreferrer" href='https://www.palisadestahoe.com/plan-your-visit/lodging' target="_blank">Village at Palisades</a></p><p style={{paddingLeft: "20px"}}><FontAwesomeIcon icon={faDirections} style={{fontSize: '20px'}} className="iconFA directions" />  - 1750 Village East Rd, Olympic Valley, CA 96146</p>
+    
+                    <p className="acLink"><a rel="noreferrer" href='https://www.palisadestahoe.com/plan-your-visit/lodging' target="_blank">Village at Palisades</a></p><p style={{paddingLeft: "20px"}}></p>
                     <p className="acLink"><a rel="noreferrer" href='https://www.airbnb.com/olympic-valley-ca/stays' target="_blank">Airbnb</a></p>
                     <p className="acLink"><a rel="noreferrer" href='https://www.vrbo.com/vacation-rentals/usa/california/lake-tahoe-ca/north-shore/squaw-valley' target="_blank">Vrbo</a></p>
                     </p>
-                    <div className='column'><p className='mapTextTitle'>Ceremony Location</p><a href='https://www.google.com/maps/dir//Olympic+Village+Lodge,+1901+Chamonix+Pl,+Olympic+Valley,+CA+96146' target='_blank' rel="noreferrer" className='mapDirections'><FontAwesomeIcon icon={faDirections} className="iconFA directions" /><span className='mapLink'>1901 Chamonix Pl, Olympic Valley, CA 96146</span></a><iframe title='mapOVC' className='map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3092.0033952619506!2d-120.2354779!3d39.1973716!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809bd93153d07aa7%3A0x840ba1fced7b1670!2sOlympic%20Village%20Lodge!5e0!3m2!1sen!2sus!4v1637384154321!5m2!1sen!2sus" aria-hidden="false" ></iframe></div>
+                  <div className='column'><p className='mapTextTitle'>Ceremony Location</p><a href='https://www.google.com/maps/dir//Olympic+Village+Lodge,+1901+Chamonix+Pl,+Olympic+Valley,+CA+96146' target='_blank' rel="noreferrer" className='mapDirections'><FontAwesomeIcon icon={faDirections} className="iconFA directions" /><span className='mapLink'>1901 Chamonix Pl, Olympic Valley, CA 96146</span></a><iframe title='mapOVC' className='map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3092.0033952619506!2d-120.2354779!3d39.1973716!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809bd93153d07aa7%3A0x840ba1fced7b1670!2sOlympic%20Village%20Lodge!5e0!3m2!1sen!2sus!4v1637384154321!5m2!1sen!2sus" aria-hidden="false" ></iframe></div>
                   </div>
-                </div>
+                 </div>
                 </div>
               </div>
       
